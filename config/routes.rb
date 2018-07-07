64c7fda7afa8199e770ac002aaa1e5ed996f9551
Katharina Peter <gaz@gmx.net>
2018-07-07 15:57:38 -0700

    added user model and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :tasks, except: %i[show edit new]
   resources :categories, only: %i[create]
   root to: 'tasks#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_01_031554) do
+ActiveRecord::Schema.define(version: 2018_07_07_223243) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,18 @@ ActiveRecord::Schema.define(version: 2018_07_01_031554) do
     t.date "date"
     t.decimal "duration_in_hours"
     t.index ["category_id"], name: "index_tasks_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
